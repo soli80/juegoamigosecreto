069a7b4b2ede7f4c94556a6fe96b196cb36b30c7
soli <solimotorola@gmail.com>
2025-08-17 17:51:02 -0300

    2025-08-17 se carga el Juego final

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresIngresados = [];
+let resultado = "";
+
+function agregarAmigo() {
+  const input = document.getElementById("nombreInput");
+  const nombre = input.value.trim();
+  if (typeof nombre !== 'string' || nombre.length < 2) {
+    alert("Debe ingresar un nombre valido, que tenga minimo dos letras.");
+    return;
+  }
+
+  nombresIngresados.push(nombre);
+  mostrarNombres();
+  input.value = "";
+}
+
+function mostrarNombres() {
+  const lista = document.getElementById("listaNombres");
+  lista.innerHTML = "";
+  nombresIngresados.forEach(nombre => {
+    const p = document.createElement("p");
+    p.textContent = nombre;
+    lista.appendChild(p);
+    });
+}
+
+function sortearAmigo() {
+    if (nombresIngresados.length === 0) return;
+
+    const indice = Math.floor(Math.random() * nombresIngresados.length);
+    resultado = nombresIngresados[indice];
+
+    const amigoSorteado = "El amigo secreto sorteado es: " + resultado;
+
+    document.getElementById("listaNombres").style.display = "none";
+    document.getElementById("sortearBtn").style.display = "none";
+
+    const resultadoDiv = document.getElementById("resultadoFinal");
+    resultadoDiv.innerHTML = `<p>${amigoSorteado}</p>`;
+
+    document.getElementById("reiniciarBtn").style.display = "inline";
+}
+
+//función adicional------------------------
+function reiniciar() {
+
+    nombresIngresados = [];
+    resultado = "";
+
+    document.getElementById("listaNombres").innerHTML = "";
+    document.getElementById("listaNombres").style.display = "block";
+    document.getElementById("resultadoFinal").innerHTML = "";
+    document.getElementById("sortearBtn").style.display = "flex";
+    document.getElementById("reiniciarBtn").style.display = "none";
+}
